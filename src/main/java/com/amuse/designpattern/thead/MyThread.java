09f9d6c05e7ec97ffd33950f3da0a4e3686d428b
liupeizhen <liupeizhen@baiwang.com>
2021-06-25 12:16:30 +0800

    1

---
+++ src/main/java/com/amuse/designpattern/thead/MyThread.java
@@ -0,0 +1,10 @@
+package com.amuse.designpattern.thead;
+/**
+ *@ClassName MyThread
+ *@Description TODO
+ *@Author 刘培振
+ *@Date 2021-06-21 19:59
+ *@Version 1.0
+ */
+public class MyThread {
+}
---
+++ src/main/java/com/amuse/designpattern/thead/MyThread2.java
@@ -0,0 +1,10 @@
+package com.amuse.designpattern.thead;
+/**
+ *@ClassName MyThread2
+ *@Description TODO
+ *@Author 刘培振
+ *@Date 2021-06-21 20:39
+ *@Version 1.0
+ */
+public class MyThread2 {
+}
