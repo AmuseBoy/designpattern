b60cb58c79ae240a8cccf312bae612f0e7e22dc7
AmuseBoy <552852310@qq.com>
2022-03-31 21:53:23 +0800

    1

--- src/main/java/com/amuse/designpattern/thead/MyThread.java
+++ src/main/java/com/amuse/designpattern/thead/MyThread.java
@@ -1,10 +1,45 @@
 package com.amuse.designpattern.thead;
+
 /**
- *@ClassName MyThread
- *@Description TODO
- *@Author 刘培振
- *@Date 2021-06-21 19:59
- *@Version 1.0
+ * @ClassName MyThread
+ * @Description TODO
+ * @Author 刘培振
+ * @Date 2021-06-21 19:59
+ * @Version 1.0
  */
-public class MyThread {
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < 300000; i++) {
+            System.out.println("i=" + (i + 1));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            MyThread myThread = new MyThread();
+            myThread.start();
+            Thread.sleep(1000);
+            myThread.interrupt();
+//            Thread.currentThread().interrupt();
+//            System.out.println("是否停止1？ = " + myThread.interrupted());
+//            System.out.println("是否停止2？ = " + myThread.interrupted());
+            System.out.println("是否停止1？ = " + myThread.isInterrupted());
+            System.out.println("是否停止2？ = " + myThread.isInterrupted());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static void main(String[] args) {
+//        Thread.currentThread().interrupt();
+//        System.out.println("是否停止1？ = " + Thread.interrupted());
+//        System.out.println("是否停止2？ = " + Thread.interrupted());
+//    }
+
+
+
 }
--- src/main/java/com/amuse/designpattern/thead/MyThread2.java
+++ src/main/java/com/amuse/designpattern/thead/MyThread2.java
@@ -1,10 +1,42 @@
 package com.amuse.designpattern.thead;
+
 /**
- *@ClassName MyThread2
- *@Description TODO
- *@Author 刘培振
- *@Date 2021-06-21 20:39
- *@Version 1.0
+ * @ClassName MyThread2
+ * @Description TODO
+ * @Author 刘培振
+ * @Date 2021-06-21 20:39
+ * @Version 1.0
  */
-public class MyThread2 {
+public class MyThread2 extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            for (int i = 0; i < 500000; i++) {
+                if(this.interrupted()){
+                    System.out.println("已经是停止状态了！我要退出了！");
+                    throw new InterruptedException();
+                }
+                System.out.println("i = " + (i+1));
+            }
+            System.out.println("我在for下面");
+        } catch (InterruptedException e) {
+            System.out.println("被捕捉到了");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyThread2 thread = new MyThread2();
+            thread.start();
+            Thread.sleep(2000);
+            thread.interrupt();
+        } catch (InterruptedException e) {
+            System.out.println("main catch");
+            e.printStackTrace();
+        }
+        System.out.println("end!");
+    }
 }
